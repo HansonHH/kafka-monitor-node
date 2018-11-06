4c2d27329caeef965954e2ae568b450e80c05b18
Xin Han <xin.han@netinsight.net>
2018-11-06 23:12:53 +0100

    Add fake logger to utils

---
+++ utils/logger.ts
@@ -0,0 +1,8 @@
+export const logger = {
+    // tslint:disable:no-console
+    info: console.log,
+    error: console.error,
+    warn: console.warn,
+    debug: console.debug
+    // tslint:enable:no-console
+}
