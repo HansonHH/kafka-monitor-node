3b7c370e8ab605e23b275ddcfa7157770efa505d
Xin Han <xin.han@netinsight.net>
2018-11-21 22:58:43 +0100

    Test fillUpRecord function in utils test

---
+++ test/utils.test.ts
@@ -0,0 +1,33 @@
+import 'mocha'
+import { expect } from 'chai'
+import { fillUpRecord } from '../lib/utils'
+
+describe('utils test', () => {
+    describe('fillUpRecord function', () => {
+        it('recordSizeByte > original record size', () => {
+            const recordSizeByte = 100
+            const record = { timestamp: Date.now(), dummy: '' }
+            const originalRecordSizeByte = Buffer.from(JSON.stringify(record)).length
+
+            const newRecord = fillUpRecord(record, recordSizeByte)
+            const newRecordSizeByte = Buffer.from(JSON.stringify(newRecord)).length
+
+            expect(Buffer.from(newRecord.dummy).length).to.equal(recordSizeByte - originalRecordSizeByte)
+            expect(newRecordSizeByte).to.equal(recordSizeByte)
+        })
+
+        it('recordSizeByte < original record size', () => {
+            const recordSizeByte = 10
+            const record = { timestamp: Date.now(), dummy: '' }
+            const originalRecordSizeByte = Buffer.from(JSON.stringify(record)).length
+
+            const newRecord = fillUpRecord(record, recordSizeByte)
+            const newRecordSizeByte = Buffer.from(JSON.stringify(newRecord)).length
+
+
+            expect(Buffer.from(newRecord.dummy).length).not.to.equal(recordSizeByte - originalRecordSizeByte)
+            expect(newRecordSizeByte).not.to.equal(recordSizeByte)
+            expect(newRecordSizeByte).to.equal(originalRecordSizeByte)
+        })
+    })
+})
